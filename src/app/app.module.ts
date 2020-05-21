f85776cb6aa7db91218dd947b5d58728f156ca82
Xy718 <869839000@qq.com>
2020-05-21 08:34:14 +0800

    嘤嘤嘤，giao！

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      TopToolbarComponent
+   ],
+   imports: [
+      AppRoutingModule,
+      BrowserModule,
+      BrowserAnimationsModule,
+      MatToolbarModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
